62a496ba2063b90dcd9e88375d35471b0f6ac918
Hazel <hazelching0609@gmail.com>
2026-01-27 08:44:21 +0100

    count collected star

--- Assets/Scripts/CollectStar.cs
+++ Assets/Scripts/CollectStar.cs
@@ -2,12 +2,14 @@ using UnityEngine;
 
 public class CollectStar : MonoBehaviour
 {
+    [SerializeField] private int starNum = 0;
     void OnTriggerEnter(Collider other)
     {
         var star = other.GetComponent<Star>();
         if (star != null)
         {
             star.Collect();
+            starNum++;
         }
     }
 }
