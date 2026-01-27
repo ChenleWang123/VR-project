7627c58c8005508756fff4f341c13a663ff331b2
shangshu0 <13395743@qq.com>
2026-01-27 19:05:47 +0100

    新建2个透明的星、星星被吃音效、星星旋转

--- Assets/Scripts/Star.cs
+++ Assets/Scripts/Star.cs
@@ -2,8 +2,20 @@ using UnityEngine;
 
 public class Star : MonoBehaviour
 {
+    public AudioClip starSound; // star被收集的音效
+    public float volume = 1.0f;
+    public Vector3 rotationAxis = Vector3.up; // 控制旋转轴  
+    public float rotationSpeed = 50f; // 控制旋转速度 
+    void Update()
+    {
+        // 围绕指定的轴旋转  
+        transform.Rotate(rotationAxis, rotationSpeed * Time.deltaTime);
+    }
+
     public void Collect()
     {
+        // 播放音效  
+        AudioSource.PlayClipAtPoint(starSound, Camera.main.transform.position, volume);
         Destroy(gameObject);
     }
 }
